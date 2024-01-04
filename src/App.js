6bdf464b910a0fe0e1197daf6309d5b7c645649e
Natcod <107844070+Natcod@users.noreply.github.com>
2024-01-05 00:49:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,210 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import cr7Image from "./img/cr7.jpg";
 
-function App() {
+export default function App() {
+  const [zIndex, setZIndex] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card path={cr7Image} alt="Cristiano Ronaldo" zIndex={zIndex}>
+        <TextExpander onIndex={setZIndex}>
+          Cristiano Ronaldo, often referred to as CR7, is one of the most
+          renowned and successful football (soccer) players in the history of
+          the sport. Born on February 5, 1985, in Madeira, Portugal, Ronaldo's
+          journey to becoming a football legend is remarkable. Here's a concise
+          history of Cristiano Ronaldo's career: Early Life: Ronaldo showed
+          exceptional talent from a young age. He began playing football for
+          local teams in Madeira and later joined Sporting Lisbon's youth
+          academy in Portugal, where his skills flourished. Rise to Prominence
+          at Manchester United: In 2003, Cristiano Ronaldo caught the attention
+          of Manchester United manager Sir Alex Ferguson during a pre-season
+          friendly match. Shortly after, he signed with Manchester United, where
+          he truly rose to prominence. Ronaldo's incredible speed, skill, and
+          goal-scoring ability helped Manchester United win numerous titles,
+          including three English Premier League titles and the UEFA Champions
+          League. Real Madrid Era and Records: In 2009, Cristiano Ronaldo
+          transferred to Real Madrid for a then-world record fee. His tenure at
+          Real Madrid was phenomenally successful. Ronaldo became the club's
+          all-time leading goal scorer, won multiple La Liga titles, and secured
+          four Champions League titles with the team. He also claimed multiple
+          Ballon d'Or awards during his time in Spain, recognizing him as the
+          world's best player. Juventus and Continued Success: In 2018, Ronaldo
+          moved to Juventus in Serie A, the top tier of Italian football.
+          Despite a change in league and team, Ronaldo continued to showcase his
+          exceptional skills, contributing significantly to Juventus' successes.
+          Return to Manchester United and Legacy: In 2021, Cristiano Ronaldo
+          made a highly anticipated return to Manchester United, where he
+          previously achieved immense success. His return was met with
+          excitement from fans worldwide, and he continued to display his
+          goal-scoring prowess for the club. Throughout his career, Ronaldo has
+          set numerous records, won numerous individual awards, and established
+          himself as one of the greatest footballers of all time. His
+          dedication, work ethic, athleticism, and consistent performances on
+          the field have solidified his place in football history. Off the
+          field, Cristiano Ronaldo is also known for his philanthropy, business
+          ventures, and strong influence on social media, making him a global
+          icon beyond the realm of football.
+        </TextExpander>
+      </Card>
+      <Card path={cr7Image} alt="Cristiano Ronaldo" zIndex={zIndex}>
+        <TextExpander onIndex={setZIndex}>
+          Cristiano Ronaldo, often referred to as CR7, is one of the most
+          renowned and successful football (soccer) players in the history of
+          the sport. Born on February 5, 1985, in Madeira, Portugal, Ronaldo's
+          journey to becoming a football legend is remarkable. Here's a concise
+          history of Cristiano Ronaldo's career: Early Life: Ronaldo showed
+          exceptional talent from a young age. He began playing football for
+          local teams in Madeira and later joined Sporting Lisbon's youth
+          academy in Portugal, where his skills flourished. Rise to Prominence
+          at Manchester United: In 2003, Cristiano Ronaldo caught the attention
+          of Manchester United manager Sir Alex Ferguson during a pre-season
+          friendly match. Shortly after, he signed with Manchester United, where
+          he truly rose to prominence. Ronaldo's incredible speed, skill, and
+          goal-scoring ability helped Manchester United win numerous titles,
+          including three English Premier League titles and the UEFA Champions
+          League. Real Madrid Era and Records: In 2009, Cristiano Ronaldo
+          transferred to Real Madrid for a then-world record fee. His tenure at
+          Real Madrid was phenomenally successful. Ronaldo became the club's
+          all-time leading goal scorer, won multiple La Liga titles, and secured
+          four Champions League titles with the team. He also claimed multiple
+          Ballon d'Or awards during his time in Spain, recognizing him as the
+          world's best player. Juventus and Continued Success: In 2018, Ronaldo
+          moved to Juventus in Serie A, the top tier of Italian football.
+          Despite a change in league and team, Ronaldo continued to showcase his
+          exceptional skills, contributing significantly to Juventus' successes.
+          Return to Manchester United and Legacy: In 2021, Cristiano Ronaldo
+          made a highly anticipated return to Manchester United, where he
+          previously achieved immense success. His return was met with
+          excitement from fans worldwide, and he continued to display his
+          goal-scoring prowess for the club. Throughout his career, Ronaldo has
+          set numerous records, won numerous individual awards, and established
+          himself as one of the greatest footballers of all time. His
+          dedication, work ethic, athleticism, and consistent performances on
+          the field have solidified his place in football history. Off the
+          field, Cristiano Ronaldo is also known for his philanthropy, business
+          ventures, and strong influence on social media, making him a global
+          icon beyond the realm of football.
+        </TextExpander>
+      </Card>
+      <Card path={cr7Image} alt="Cristiano Ronaldo" zIndex={zIndex}>
+        <TextExpander onIndex={setZIndex}>
+          Cristiano Ronaldo, often referred to as CR7, is one of the most
+          renowned and successful football (soccer) players in the history of
+          the sport. Born on February 5, 1985, in Madeira, Portugal, Ronaldo's
+          journey to becoming a football legend is remarkable. Here's a concise
+          history of Cristiano Ronaldo's career: Early Life: Ronaldo showed
+          exceptional talent from a young age. He began playing football for
+          local teams in Madeira and later joined Sporting Lisbon's youth
+          academy in Portugal, where his skills flourished. Rise to Prominence
+          at Manchester United: In 2003, Cristiano Ronaldo caught the attention
+          of Manchester United manager Sir Alex Ferguson during a pre-season
+          friendly match. Shortly after, he signed with Manchester United, where
+          he truly rose to prominence. Ronaldo's incredible speed, skill, and
+          goal-scoring ability helped Manchester United win numerous titles,
+          including three English Premier League titles and the UEFA Champions
+          League. Real Madrid Era and Records: In 2009, Cristiano Ronaldo
+          transferred to Real Madrid for a then-world record fee. His tenure at
+          Real Madrid was phenomenally successful. Ronaldo became the club's
+          all-time leading goal scorer, won multiple La Liga titles, and secured
+          four Champions League titles with the team. He also claimed multiple
+          Ballon d'Or awards during his time in Spain, recognizing him as the
+          world's best player. Juventus and Continued Success: In 2018, Ronaldo
+          moved to Juventus in Serie A, the top tier of Italian football.
+          Despite a change in league and team, Ronaldo continued to showcase his
+          exceptional skills, contributing significantly to Juventus' successes.
+          Return to Manchester United and Legacy: In 2021, Cristiano Ronaldo
+          made a highly anticipated return to Manchester United, where he
+          previously achieved immense success. His return was met with
+          excitement from fans worldwide, and he continued to display his
+          goal-scoring prowess for the club. Throughout his career, Ronaldo has
+          set numerous records, won numerous individual awards, and established
+          himself as one of the greatest footballers of all time. His
+          dedication, work ethic, athleticism, and consistent performances on
+          the field have solidified his place in football history. Off the
+          field, Cristiano Ronaldo is also known for his philanthropy, business
+          ventures, and strong influence on social media, making him a global
+          icon beyond the realm of football.
+        </TextExpander>
+      </Card>
+      <Card path={cr7Image} alt="Cristiano Ronaldo" zIndex={zIndex}>
+        <TextExpander onIndex={setZIndex}>
+          Cristiano Ronaldo, often referred to as CR7, is one of the most
+          renowned and successful football (soccer) players in the history of
+          the sport. Born on February 5, 1985, in Madeira, Portugal, Ronaldo's
+          journey to becoming a football legend is remarkable. Here's a concise
+          history of Cristiano Ronaldo's career: Early Life: Ronaldo showed
+          exceptional talent from a young age. He began playing football for
+          local teams in Madeira and later joined Sporting Lisbon's youth
+          academy in Portugal, where his skills flourished. Rise to Prominence
+          at Manchester United: In 2003, Cristiano Ronaldo caught the attention
+          of Manchester United manager Sir Alex Ferguson during a pre-season
+          friendly match. Shortly after, he signed with Manchester United, where
+          he truly rose to prominence. Ronaldo's incredible speed, skill, and
+          goal-scoring ability helped Manchester United win numerous titles,
+          including three English Premier League titles and the UEFA Champions
+          League. Real Madrid Era and Records: In 2009, Cristiano Ronaldo
+          transferred to Real Madrid for a then-world record fee. His tenure at
+          Real Madrid was phenomenally successful. Ronaldo became the club's
+          all-time leading goal scorer, won multiple La Liga titles, and secured
+          four Champions League titles with the team. He also claimed multiple
+          Ballon d'Or awards during his time in Spain, recognizing him as the
+          world's best player. Juventus and Continued Success: In 2018, Ronaldo
+          moved to Juventus in Serie A, the top tier of Italian football.
+          Despite a change in league and team, Ronaldo continued to showcase his
+          exceptional skills, contributing significantly to Juventus' successes.
+          Return to Manchester United and Legacy: In 2021, Cristiano Ronaldo
+          made a highly anticipated return to Manchester United, where he
+          previously achieved immense success. His return was met with
+          excitement from fans worldwide, and he continued to display his
+          goal-scoring prowess for the club. Throughout his career, Ronaldo has
+          set numerous records, won numerous individual awards, and established
+          himself as one of the greatest footballers of all time. His
+          dedication, work ethic, athleticism, and consistent performances on
+          the field have solidified his place in football history. Off the
+          field, Cristiano Ronaldo is also known for his philanthropy, business
+          ventures, and strong influence on social media, making him a global
+          icon beyond the realm of football.
+        </TextExpander>
+      </Card>
     </div>
   );
 }
+function Card({ path, alt, zIndex, children }) {
+  return (
+    <div className="card" style={(zIndex = { zIndex })}>
+      <div>
+        <div className="img-container">
+          <ImageDisplay path={path} alt={alt} />
+        </div>
+        <div>
+          <p>{children}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+function TextExpander({ expanded = false, onIndex, children }) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
 
-export default App;
+  function handleClick() {
+    setIsExpanded(!isExpanded);
+    isExpanded ? onIndex(0) : onIndex(1);
+  }
+  const displayText = isExpanded
+    ? children
+    : children.split(" ").slice(0, 40).join(" ") + "...";
+  return (
+    <div>
+      {displayText}
+      <button onClick={handleClick}>
+        {isExpanded ? "Read Less" : "Read More"}
+      </button>
+    </div>
+  );
+}
+function ImageDisplay({ path, alt }) {
+  return (
+    <>
+      <img src={path} alt={alt} />
+    </>
+  );
+}
